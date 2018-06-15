6f0c91cb5e80bfb8ddcde98d62a2a8d63670427f
David V. Lu <dlu@locusrobotics.com>
2018-06-15 17:17:03 -0400

    Working

---
+++ ros_buildfarm/aggregate_status.py
@@ -0,0 +1,196 @@
+import re
+import collections
+
+CANDIDATES = ['build', 'test', 'main']
+VERSION_PATTERN = re.compile('([\d\-\.]+)\w+.*')
+
+
+def map_check(M, needle, key):
+    return M[key] == set(needle)
+
+
+def some_value_check(M, needle):
+    if type(needle) != set:
+        needle = set(needle)
+    for key in M.keys():
+        if M[key] == needle:
+            return True
+    return False
+
+
+def some_other_value_check(M, needle, exclude):
+    if type(needle) != set:
+        needle = set(needle)
+    for key in M.keys():
+        if key == exclude:
+            continue
+        if M[key] == needle:
+            return True
+    return False
+
+
+def sub_filter(D, o_filter=None, d_filter=None, b_filter=None, c_filter=None):
+    D2 = {}
+    for os_name, os_d in D.iteritems():
+        if o_filter == os_name:
+            continue
+        D2[os_name] = {}
+        for os_distro, osd_d in os_d.iteritems():
+            if d_filter == os_distro:
+                continue
+            D2[os_name][os_distro] = {}
+            for binary_type, b_d in osd_d.iteritems():
+                if b_filter == binary_type:
+                    continue
+                D2[os_name][os_distro][binary_type] = {}
+                for candidate, v in b_d.iteritems():
+                    if c_filter == candidate:
+                        continue
+                    D2[os_name][os_distro][binary_type][candidate] = v
+
+    return {'build_status': D2}
+
+
+def get_distro_status(D, expected, blacklist, candidates=CANDIDATES, skip_source=False):
+    version_map = collections.defaultdict(list)
+    os_map = collections.defaultdict(set)
+    distro_map = collections.defaultdict(set)
+    build_map = collections.defaultdict(set)
+    level_map = collections.defaultdict(set)
+    combo_map = collections.defaultdict(set)
+
+    for os_name in expected:
+        os_d = D['build_status'].get(os_name, {})
+        for os_distro in expected[os_name]:
+            osd_d = os_d.get(os_distro, {})
+            for binary_type in expected[os_name][os_distro]:
+                if skip_source and binary_type == 'source':
+                    continue
+                b_d = osd_d.get(binary_type, {})
+                for candidate in candidates:
+                    version = None
+                    if candidate in b_d:
+                        version = VERSION_PATTERN.match(b_d[candidate]).group(1)
+                    elif (os_name, os_distro, binary_type) in blacklist:
+                        continue
+                    os_map[version].add(os_name)
+                    distro_map[version].add(os_distro)
+                    build_map[version].add(binary_type)
+                    level_map[version].add(candidate)
+                    combo_map[version].add(os_distro + '/' + binary_type)
+                    version_map[version].append((os_name, os_distro, binary_type, candidate))
+    counts = {}
+    for name, d in [('os', os_map), ('distro', distro_map), ('build_type', build_map), ('level', level_map)]:
+        counts[name] = len(d)
+
+    if sum(counts.values()) == 4:
+        return 'released'
+
+    if counts['level'] == 2:
+        if map_check(level_map, ['main'], None):
+            return 'waiting for new release'
+        elif some_value_check(level_map, ['main']):
+            return 'waiting for re-release'
+    elif counts['level'] == 3:
+        if map_check(level_map, ['main'], None) and some_other_value_check(level_map, ['main'], None):
+            return 'waiting for new/re-release'
+
+    if counts['build_type'] == 2:
+        a, b = sorted(build_map.keys())
+        if map_check(build_map, ['source'], a):
+            return 'source builds, binary doesn\'t'
+        elif b is not None and map_check(build_map, ['source'], b):
+            return 'source builds, binary doesn\'t'
+
+    if counts['distro'] == 2:
+        a, b = distro_map.values()
+        if len(a.intersection(b)) == 0 and None in distro_map:
+            value = distro_map[None]
+            return 'does not build on ' + ', '.join(list(value))
+
+    if counts['build_type'] == 2:
+        a, b = build_map.values()
+        if len(a.intersection(b)) == 0 and None in build_map:
+            value = build_map[None]
+            return 'does not build on ' + ', '.join(list(value))
+
+    if len(combo_map) == 2:
+        a, b = combo_map.values()
+        if len(a.intersection(b)) == 0 and None in combo_map:
+            value = combo_map[None]
+            return 'does not build on ' + ', '.join(list(value))
+
+    if None in version_map:
+        values = version_map[None]
+        DX = set([(os_distro, binary_type) for os_name, os_distro, binary_type, candidate in values])
+        if len(DX) == 1:
+            a, b = list(DX)[0]
+            return "doesn't build on %s/%s" % (a, b)
+
+    if candidates == CANDIDATES:
+        sub_candidates = CANDIDATES[:-1]  # skip main
+        status = get_distro_status(D, expected, blacklist, sub_candidates)
+        if status and status != 'released':
+            return status
+
+        status = get_distro_status(D, expected, blacklist, sub_candidates, True)
+        if status and status != 'released':
+            return 'binary: ' + status
+
+    if len(combo_map) == 2 and None in combo_map:
+        value = combo_map[None]
+        return 'does not build on ' + ', '.join(list(value))
+
+    print dict(os_map)
+    print dict(distro_map)
+    print dict(build_map)
+    print dict(level_map)
+    print dict(combo_map)
+    print counts
+    for k, v in version_map.items():
+        for m in v:
+            print k, m
+    print
+    # exit(0)
+
+    """
+    if counts['level'] == 3:
+        vs = sorted(level_map.keys(), cmp=version_sort)
+        if map_check(level_map, ['public'], vs[0]) and \
+           map_check(level_map, ['public'], vs[1]) and \
+           map_check(level_map, ['shadow', 'build'], vs[2]):
+            return 'waiting for first full release'
+
+    if counts['distro'] == 2:
+        a, b = distro_map.values()
+        if len(a) == 1:
+            return 'trouble building ' + list(a)[0]
+
+    if counts['level'] >= 3:
+        return 'multiple versions'
+
+    if whiny:
+        print '\n'
+        print counts['build_type']
+        for k, v in the_maps.iteritems():
+            print k
+            for a, b in v.iteritems():
+                print ' ', a, b
+        for v in self.version_map:
+            print v, len(self.version_map[v])
+            if v == 'None':
+                print self.version_map[v]
+        return 'unknown'
+    return self.status
+    """
+
+
+def get_aggregate_status(D, expected, pkg_name=None, blacklist={}):
+    per_distro = {}
+    for distro in sorted(D):
+        if distro == 'maintainers':
+            continue
+        status = get_distro_status(D[distro], expected[distro], blacklist.get(pkg_name, {}).get(distro, set()))
+        per_distro[distro] = status
+
+    return per_distro
---
+++ ros_buildfarm/super_status.py
@@ -0,0 +1,183 @@
+import collections
+import os
+import requests
+import re
+import yaml
+from .aggregate_status import get_aggregate_status
+from .config import get_index as get_config_index, get_release_build_files
+from .templates import expand_template
+
+YAML_FOLDER = 'http://repositories.ros.org/status_page/yaml/'
+YAML_PATTERN = re.compile('<a href="(ros_(\w+)_(\w+).yaml)">')
+GITHUB_PATTERN = re.compile('https?://github.com/([^/]+)/(.+)\.git')
+GITHUB_BRANCH_PATTERN = re.compile('https://github.com/([^/]+)/([^/]+)/tree/(.*)')
+BB_PATTERN = re.compile('https://bitbucket.org/(.*)/(.*)')
+GITLAB_PATTERN = re.compile('https?://gitlab.[^/]+/([^/]+)/(.+).git')
+URL_PATTERNS = [GITHUB_PATTERN, GITHUB_BRANCH_PATTERN, BB_PATTERN, GITLAB_PATTERN]
+
+
+def get_yaml_filenames():
+    filenames = {}
+    r = requests.get(YAML_FOLDER)
+    for filename, distro, machine in YAML_PATTERN.findall(r.text):
+        filenames[distro, machine] = filename
+    return filenames
+
+
+def dict_merge(dct, merge_dct):
+    for k, v in merge_dct.iteritems():
+        if (k in dct and isinstance(dct[k], dict) and isinstance(merge_dct[k], collections.Mapping)):
+            dict_merge(dct[k], merge_dct[k])
+        else:
+            dct[k] = merge_dct[k]
+
+
+def merge_status_yaml(data, new_data, new_distro):
+    for pkg, D in new_data.items():
+        D2 = {new_distro: {}, 'maintainers': {}}
+        for k, v in D.iteritems():
+            if k == 'maintainers':
+                for d in v:
+                    D2['maintainers'][d['email']] = d['name']
+            else:
+                D2[new_distro][k] = v
+
+        if pkg in data:
+            dict_merge(data[pkg], D2)
+        else:
+            data[pkg] = D2
+
+
+def get_multi_distro_status(distros):
+    status = {}
+    for (distro, machine), filename in sorted(get_yaml_filenames().items()):
+        print repr(distro), distros
+        if distro not in distros:
+            continue
+        print('Loading {}/{}'.format(distro, machine))
+        r = requests.get(YAML_FOLDER + filename)
+        distro_status = yaml.load(r.text)
+        merge_status_yaml(status, distro_status, distro)
+    return status
+
+
+def get_url_fields(s):
+    for pattern in URL_PATTERNS:
+        m = pattern.match(s)
+        if m:
+            return m.groups()
+
+
+def get_organization_and_repo(entry):
+    """
+       Iterates through the different distributions and returns the org and repo for the most recent distro
+    """
+    url = None
+    for key, distro_dict in sorted(entry.items(), reverse=True):
+        if 'url' not in distro_dict:
+            continue
+        url = distro_dict['url']
+        fields = get_url_fields(url)
+        if fields is not None:
+            return fields[0], fields[1]
+    # If nothing found, return organization=None and repo=full url
+    return None, url
+
+
+def get_blacklist(build_file_dict):
+    blacklist = collections.defaultdict(lambda: collections.defaultdict(set))
+
+    for distro in sorted(build_file_dict):
+        for machine in build_file_dict[distro]:
+            build_file = build_file_dict[distro][machine]
+            if len(build_file.package_blacklist) == 0:
+                continue
+            for pkg in build_file.package_blacklist:
+                for os_name, os_d in build_file.targets.items():
+                    for os_flavor, fl_d in os_d.items():
+                        for build in fl_d:
+                            blacklist[pkg][distro].add((os_name, os_flavor, build))
+    return dict(blacklist)
+
+
+def collect_expected_values(build_file_dict):
+    C = collections.defaultdict(lambda: collections.defaultdict(lambda: collections.defaultdict(set)))
+    for distro in build_file_dict:
+        for machine in build_file_dict[distro]:
+            build_file = build_file_dict[distro][machine]
+            for os_name, os_d in build_file.targets.items():
+                for os_flavor, fl_d in os_d.items():
+                    C[distro][os_name][os_flavor].add('source')
+                    for build in fl_d:
+                        C[distro][os_name][os_flavor].add(build)
+    return C
+
+
+def build_super_status_page(config_url, output_dir='.', distros=[]):
+    config = get_config_index(config_url)
+    if len(distros) == 0:
+        distros = list(config.distributions.keys())
+
+    build_file_dict = {}
+    for distro in distros:
+        build_file_dict[distro] = get_release_build_files(config, distro)
+
+    multi_distro_status = get_multi_distro_status(distros)
+    print('Write yaml file')
+    yaml_filename = os.path.join(output_dir, 'multi_distro_status.yaml')
+    yaml.safe_dump(multi_distro_status, open(yaml_filename, 'w'), allow_unicode=True)
+
+    print('Examining each package')
+    super_status = {}
+    blacklist = get_blacklist(build_file_dict)
+    expected = collect_expected_values(build_file_dict)
+    for pkg, entry in sorted(multi_distro_status.items()):
+        org, repo = get_organization_and_repo(entry)
+        if org not in super_status:
+            super_status[org] = {'repos': {}}
+        org_dict = super_status[org]['repos']
+        if repo not in org_dict:
+            org_dict[repo] = {'pkgs': {}}
+        repo_dict = org_dict[repo]['pkgs']
+        status = get_aggregate_status(entry, expected, pkg, blacklist)
+        d = {'status': status}
+        if 'maintainers' in entry:
+            d['maintainers'] = entry['maintainers']
+        repo_dict[pkg] = d
+
+    print('Getting status for each org/repo')
+    for org, org_dict in super_status.items():
+        org_set = collections.defaultdict(set)
+        for repo, repo_dict in org_dict['repos'].items():
+            repo_set = collections.defaultdict(set)
+            for pkg, pkg_dict in repo_dict['pkgs'].items():
+                for distro, status in pkg_dict['status'].iteritems():
+                    org_set[distro].add(status)
+                    repo_set[distro].add(status)
+            repo_dict['status'] = {}
+            for distro, statuses in repo_set.items():
+                d_status = None
+                if len(statuses) == 1:
+                    d_status = list(statuses)[0]
+                else:
+                    d_status = 'mixed'
+                repo_dict['status'][distro] = d_status
+        org_dict['status'] = {}
+        for distro, statuses in org_set.items():
+            d_status = None
+            if len(statuses) == 1:
+                d_status = list(statuses)[0]
+            else:
+                d_status = 'mixed'
+            org_dict['status'][distro] = d_status
+
+    print('Write parsed yaml file')
+    yaml_filename = os.path.join(output_dir, 'super_status.yaml')
+    yaml.safe_dump(super_status, open(yaml_filename, 'w'), allow_unicode=True)
+
+    output_filename = os.path.join(output_dir, 'super_status.html')
+    print("Generating super status page '%s':" % output_filename)
+    template_name = 'status/super_status_page.html.em'
+    html = expand_template(template_name, super_status)
+    with open(output_filename, 'w') as h:
+        h.write(html)
